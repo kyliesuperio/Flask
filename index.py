79d84fe03bf605411a4d4e96f57944323c3a9192
kyliesuperio <160607629+kyliesuperio@users.noreply.github.com>
2024-04-06 00:38:39 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,12 @@
+# pip install flask
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return ('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
